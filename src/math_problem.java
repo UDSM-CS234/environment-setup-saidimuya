a7d5c810d77091c5021683f0a109ba3be7b2156d
saidimuya <saidimuya01@gmail.com>
2022-05-08 14:44:56 +0300

    lab1 assignment

---
+++ src/math_problem.java
@@ -0,0 +1,7 @@
+public class math_problem {
+    public static void main(String[] args){
+        double a=-9.81; double t=10; double vi=0; int Xi=0; double Xt;
+        Xt=0.5*a*t*t+vi*t+Xi;
+        System.out.println("the value of Xt is,"+Xt);
+    }
+}
